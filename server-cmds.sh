91e5190c244a7d3c38ece80066a3094726462019
SimonGPort <simon.giroux.portelance@gmail.com>
2021-11-18 17:38:30 -0500

    useless line in deploy image

--- server-cmds.sh
+++ server-cmds.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker stop $(docker ps -q --filter ancestor=simongport/react-nodejs-example:1.2)
-docker run -p 3080:3080 -d simongport/react-nodejs-example:1.3
+docker stop $(docker ps -q --filter ancestor=simongport/react-nodejs-example:1.3)
+docker run -p 3080:3080 -d simongport/react-nodejs-example:1.4
