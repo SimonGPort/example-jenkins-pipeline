c998c64e17d42508415c34c7a53e46afcce83b4f
SimonGPort <simon.giroux.portelance@gmail.com>
2021-11-18 17:33:54 -0500

    wrong ip address2

--- server-cmds.sh
+++ server-cmds.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker stop $(docker ps -q --filter ancestor=simongport/react-nodejs-example:1.1)
-docker run -p 3080:3080 -d simongport/react-nodejs-example:1.1
+docker run -p 3080:3080 -d simongport/react-nodejs-example:1.2
