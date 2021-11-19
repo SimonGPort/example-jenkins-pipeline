98ef353326a482dcc17ec35a6d547a7ae0e52861
SimonGPort <simon.giroux.portelance@gmail.com>
2021-11-19 18:08:47 -0500

    stop port running

--- server-cmds.sh
+++ server-cmds.sh
@@ -3,10 +3,11 @@
 echo "inside shell cmds"
 echo "variables $1 $2"
 
-# if[-e docker ps -q --filter ancestor=$2]
-# then
-# echo "inside if function"
-# docker stop $(docker ps -q --filter ancestor=$2)
-# fi
+if lsof -Pi :3080 -sTCP:LISTEN -t >/dev/null ; then
+    echo "port 3080 running"
+    docker stop $(docker ps -q --filter ancestor=$2)
+else
+    echo "port 3080 not running"
+fi
 
 docker run -p 3080:3080 -d $1
