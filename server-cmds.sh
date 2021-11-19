50717bef982d1ef1a6517efd0e79c34832564f27
SimonGPort <simon.giroux.portelance@gmail.com>
2021-11-19 17:02:10 -0500

    test server-cmds script

--- server-cmds.sh
+++ server-cmds.sh
@@ -2,7 +2,13 @@
 
 echo "inside shell cmds"
 echo "variables $1 $2"
-if(docker ps -q --filter ancestor=$2){
+IFVALUE=docker ps -q --filter ancestor=$2
+echo $IFVALUE
+
+if[docker ps -q --filter ancestor=$2]
+then
+echo "inside if function"
 docker stop $(docker ps -q --filter ancestor=$2)
-}
+fi
+
 docker run -p 3080:3080 -d $1
