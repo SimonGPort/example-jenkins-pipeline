8202b6e586610d15242a13fff74ae7e14675c215
SimonGPort <simon.giroux.portelance@gmail.com>
2021-11-19 18:33:25 -0500

    pipeline

--- server-cmds.sh
+++ server-cmds.sh
@@ -3,11 +3,6 @@
 echo "inside shell cmds"
 echo "variables $1 $2"
 
-if docker ps -q --filter ancestor=$2 >/dev/null ; then
-    echo "port 3080 running"
-    docker stop $(docker ps -q --filter ancestor=$2)
-else
-    echo "port 3080 not running"
-fi
+docker stop $(docker ps -q --filter ancestor=$2)
 
 docker run -p 3080:3080 -d $1
